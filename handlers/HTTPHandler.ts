93a141fa4f2e9a4fa02c7bc52620b1e3f04e45ff
JLCD <jean-luc@jlcd.de>
2023-02-05 13:22:02 +0100

    added http support

--- handlers/HTTPHandler.ts
+++ handlers/HTTPHandler.ts
@@ -1,14 +1,48 @@
+import express from "express";
+import { streamDeckConfig } from "../index";
+import { json, urlencoded } from "body-parser";
+import { post } from "axios";
+import { changeIcon } from "../utils/streamdeckUtils";
+
 export class HTTPHandler {
-  constructor() {}
-  send(adress: string, payload: string) {
-    fetch(adress, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: payload,
+  client: any;
+  routeMap: Record<string, number> = {};
+  constructor() {
+    this.client = express();
+    this.routeMap = {};
+
+    this.client.use(json());
+    this.client.use(urlencoded({ extended: false }));
+
+    this.setHandlers();
+    this.client.listen(streamDeckConfig.httpPort, () => {
+      console.log("[HTTP] Listening on port " + streamDeckConfig.httpPort);
     });
   }
-  attachListner(index: number) {}
+  send(adress: string, payload: string) {
+    post(adress, { payload });
+  }
+  setHandlers() {
+    for (const route in this.routeMap) {
+      this.client.post("/api/buttons" + route, (req: Request, res: any) => {
+        if (!req.body) return;
+        const body = req.body;
+        const index = this.routeMap[route];
+        const button = streamDeckConfig.streamdeckConfig.buttonSettings[index];
+        //@ts-ignore
+        button.typeSpecifigConfig.state = !!body.payload;
+        changeIcon(index);
+        res.status(200);
+        res.send("OK");
+      });
+    }
+  }
+
+  attachListner(index: number) {
+    const path =
+      streamDeckConfig.streamdeckConfig.buttonSettings[index].typeSpecifigConfig
+        .incomingPath;
+    this.routeMap[path] = index;
+    this.setHandlers();
+  }
 }
